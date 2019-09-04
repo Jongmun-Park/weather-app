5ff1a02228fadf4d2d33140516beac27262e0387
Jongmun-Park <momenttraveler@gmail.com>
2019-09-04 12:48:49 +0900

    add getWeather, getLocation function

--- App.js
+++ App.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
 
+const API_KEY = "40238fb4c0f33b32f75fd6fb9b990771";
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      );
+      console.log(data);
+  };
+
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false});
+    } catch (error) {
+      Alert.alert("i can't find you");
+    }
+  };
+
+  componentDidMount(){
+    this.getLocation();
+  }
+
   render() {
-    return <Loading></Loading>
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
